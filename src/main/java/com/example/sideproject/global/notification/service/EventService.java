9e054c88710ad6c0387c7b052e366584178fef35
hyun <apfhd0257@naver.com>
2025-01-09 19:40:53 +0900

    [FIX]: 알림 메시지 전송자에게 수신이 안되도록 수정

--- src/main/java/com/example/sideproject/global/notification/service/EventService.java
+++ src/main/java/com/example/sideproject/global/notification/service/EventService.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -19,18 +20,36 @@ public class EventService {
     private final SseService sseService;
     private final NotificationService notificationService;
 
+    /**
+     * sse로 알림 데이터 전송
+     * @param listDto 알림 리스트
+     */
     @EventListener
     public void send(EventListDto listDto) {
-        listDto.eventDtos().forEach(sseService::send);
+        filterEvents(listDto).forEach(sseService::send);
     }
 
+    /**
+     * 알림 데이터를 데이터베이스에 저장한다.
+     * @param listDto 알림 리스트
+     */
     @Async("taskExecutor")
     @EventListener
     public void save(EventListDto listDto) {
-        listDto.eventDtos().forEach(eventDto -> {
+        filterEvents(listDto).forEach(eventDto -> {
             NotificationRequestDto requestDto = NotificationRequestDto.of(eventDto);
             notificationService.createNotification(requestDto, new User(eventDto.from()));
         });
     }
 
+    /**
+     * 본인에게는 알림이 안가도록 필터링
+      * @param listDto 알림 리스트
+     * @return 본인 제외한 알림 리스트
+     */
+    private List<EventDto> filterEvents(EventListDto listDto) {
+        return listDto.eventDtos().stream()
+                .filter(event -> !Objects.equals(event.from(), event.to()))
+                .toList();
+    }
 }
