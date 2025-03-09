8111f2b6ba9cebe5bd2981a718d0ee76d22c792c
hyun <apfhd0257@naver.com>
2025-03-09 15:06:56 +0900

    [fix] elasticsearch disable

--- src/main/java/com/example/sideproject/domain/project/service/ProjectService.java
+++ src/main/java/com/example/sideproject/domain/project/service/ProjectService.java
@@ -7,8 +7,8 @@ import com.example.sideproject.domain.project.entity.ProjectTechStack;
 import com.example.sideproject.domain.project.entity.ProjectUrl;
 import com.example.sideproject.domain.project.repository.ProjectRepository;
 import com.example.sideproject.domain.project.repository.query.ProjectQueryRepository;
-import com.example.sideproject.domain.search.repository.SearchProjectRepository;
-import com.example.sideproject.domain.search.service.SearchService;
+//import com.example.sideproject.domain.search.repository.SearchProjectRepository;
+//import com.example.sideproject.domain.search.service.SearchService;
 import com.example.sideproject.domain.techstack.entity.TechStack;
 import com.example.sideproject.domain.techstack.repository.TechStackRepository;
 import com.example.sideproject.domain.user.entity.User;
@@ -41,8 +41,8 @@ public class ProjectService {
     private final ProjectUrlService projectUrlService;
     private final TechStackRepository techStackRepository; // 임시
     private final ProjectQueryRepository projectQueryRepository;
-    private final SearchService searchService;
-    private final SearchProjectRepository searchProjectRepository;
+//    private final SearchService searchService;
+//    private final SearchProjectRepository searchProjectRepository;
 
     /**
      * 프로젝트 구인 글 생성
@@ -53,7 +53,7 @@ public class ProjectService {
         final User foundUser = validateActiveUser(user);
         final Project project = requestDto.toEntity(foundUser);
         Project savedProject = projectRepository.save(project);
-        searchService.saveProject(savedProject);
+//        searchService.saveProject(savedProject);
 
         List<TechStack> techStacks = new ArrayList<>();
         List<Long> techStackIds = new ArrayList<>();
@@ -157,7 +157,7 @@ public class ProjectService {
 
         Project updateProject = requestDto.update(user, projectId, projectTechStacks, existImageUrls);
         Project savedProject = projectRepository.save(updateProject);
-        searchService.saveProject(savedProject);
+//        searchService.saveProject(savedProject);
     }
 
     /**
@@ -170,7 +170,7 @@ public class ProjectService {
 
         projectRepository.delete(project);
         // 이미 project를 지웠기 때문에 받은 projectId로 해당하는 엘라스틱서치의 document삭제
-        searchProjectRepository.deleteById(projectId);
+//        searchProjectRepository.deleteById(projectId);
     }
 
 
