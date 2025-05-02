9eac873872c4ba0a6ebef06bc9c3d3537fac5fa8
hyun <apfhd0257@naver.com>
2025-05-02 15:12:29 +0900

    [FEAT] PR 게시글 텍스트 조회 추가

--- src/main/java/com/example/sideproject/domain/pr/repository/query/PrQueryRepository.java
+++ src/main/java/com/example/sideproject/domain/pr/repository/query/PrQueryRepository.java
@@ -101,7 +101,11 @@ public class PrQueryRepository {
             idIn = qPr.id.in(prIds);
         }
 
-        return Expressions.allOf(positionIn, workTypeIn, idIn);
+        return Expressions.allOf(positionIn, workTypeIn, idIn, containsBySearchText(prSearchRequest.searchText()));
+    }
+
+    private BooleanExpression containsBySearchText(String searchText){
+        return qPr.title.contains(searchText).or(qPr.introduce.contains(searchText));
     }
 
     private List<Long> findIdsByTechStackIds(List<Long> techStackIds) {
