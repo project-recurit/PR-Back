3f6524b25e38e671293881aaca84dc36b238c462
hamseungwan2023 <syham001@gmail.com>
2025-10-12 16:59:07 +0900

    modify : DTO 및 쿼리DSL 날짜 형식 LOCALDATETIME으로 변경

--- src/main/java/com/example/sideproject/domain/recruitment/dto/RecruitmentCommentResponseDto.java
+++ src/main/java/com/example/sideproject/domain/recruitment/dto/RecruitmentCommentResponseDto.java
@@ -2,26 +2,41 @@ package com.example.sideproject.domain.recruitment.dto;
 
 import com.example.sideproject.domain.recruitment.entity.RecruitmentComment;
 import com.example.sideproject.global.dto.CommentResponseDto;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-public class RecruitmentCommentResponseDto extends CommentResponseDto {
-    public RecruitmentCommentResponseDto(Long commentId, String content, String nickname, String profileUrl,
-                                         int replyCount, LocalDateTime createdAt, LocalDateTime modifiedAt) {
-        super(commentId, content, nickname, profileUrl, replyCount, String.valueOf(createdAt), String.valueOf(modifiedAt));
+public class RecruitmentCommentResponseDto {
+
+    private Long commentId;
+    private String content;
+    private String nickname;
+    private String profileUrl;
+    private int replyCount;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public RecruitmentCommentResponseDto() {}
+
+    /**
+     * QueryDSL용 생성자
+     */
+    @QueryProjection
+    public RecruitmentCommentResponseDto(Long commentId,
+                                         String content,
+                                         String nickname,
+                                         String profileUrl,
+                                         int replyCount,
+                                         LocalDateTime createdAt,
+                                         LocalDateTime modifiedAt) {
+        this.commentId = commentId;
+        this.content = content;
+        this.nickname = nickname;
+        this.profileUrl = profileUrl;
+        this.replyCount = replyCount;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
     }
-//
-//    public RecruitmentCommentResponseDto(RecruitmentComment recruitmentComment) {
-//        super(
-//                recruitmentComment.getId(),
-//                recruitmentComment.getContent(),
-//                recruitmentComment.getUser().getNickname(),
-//                recruitmentComment.getUser().getProfileUrl(),
-//                recruitmentComment.getReplyCount(),
-//                recruitmentComment.getCreatedAt().toString(),
-//                recruitmentComment.getModifiedAt().toString()
-//        );
-//    }
 }
--- src/main/java/com/example/sideproject/domain/recruitment/repository/query/RecruitmentCommentQueryRepository.java
+++ src/main/java/com/example/sideproject/domain/recruitment/repository/query/RecruitmentCommentQueryRepository.java
@@ -32,8 +32,8 @@ public class RecruitmentCommentQueryRepository {
                         comment.user.nickname,
                         comment.user.profileUrl,
                         comment.replyCount,
-                        comment.createdAt.stringValue(),
-                        comment.modifiedAt.stringValue()
+                        comment.createdAt,
+                        comment.modifiedAt
                 ))
                 .from(comment)
                 .where(comment.recruitment.id.eq(recruitmentId).and(comment.parentId.isNull())) // 부모 댓글만 가져오기
