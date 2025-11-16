dbcd95baed38419fc3e859bff93c2af275f1e13a
silica <zergskybmw@gmail.com>
2025-11-16 12:11:37 +0900

    [FIX] 유저 기술 스택 추가

--- src/main/java/com/example/sideproject/domain/user/dto/ProfileResponseDto.java
+++ src/main/java/com/example/sideproject/domain/user/dto/ProfileResponseDto.java
@@ -1,9 +1,14 @@
 package com.example.sideproject.domain.user.dto;
 
 import com.example.sideproject.domain.user.entity.User;
+import com.example.sideproject.domain.user.entity.UserTechStack;
+import com.example.sideproject.global.enums.Position;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -12,6 +17,8 @@ public class ProfileResponseDto {
     private String email;
     private String profileUrl;
     private String bio;
+    private Position position;
+    private List<UserTechStack> techStacks;
 
     @Builder
     public ProfileResponseDto(User user) {
@@ -19,5 +26,7 @@ public class ProfileResponseDto {
         this.email = user.getEmail();
         this.profileUrl = user.getProfileUrl();
         this.bio = user.getBio();
+        this.position = user.getPosition();
+        this.techStacks = new ArrayList<>();
     }
 }
